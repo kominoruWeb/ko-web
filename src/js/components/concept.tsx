bea723f55b02537cd22e48f4c241a771d92ce9f5
piou <piou@hotmail.co.jp>
2021-03-10 00:54:33 +0900

    Update

--- src/js/components/concept.tsx
+++ src/js/components/concept.tsx
@@ -107,70 +107,59 @@ const ConceptLayoutImageOuterRight = styled(ConceptLayoutImageOuter)`
 
 export const Concept: FunctionComponent = () => {
   const {language} = useLanguage()
-  const {search} = useLocation()
-  const isAlt = search === '?alt'
   return <Outer>
     <ConceptItem>
       <ConceptItemInner>
         <ConceptTitle>
-          <Text ja="金額で測れない豊かな空間" en="Rich space that cannot be measured by the amount of money"/>
+          <Text
+            ja="金額で測れない豊かな空間"
+            en="Rich space that cannot be measured by the money"
+            zh="无法用金钱来衡量丰富的空间"
+          />
         </ConceptTitle>
         <ConceptText language={language}>
           <Text
-            ja={`空間の質は、必ずしも予算に比例しないことを実証し続けています。\nよい建築、よい空間は、設計のプロセスでどれほど妥協せず\n時間をかけて吟味し続けるかで決まります。\nそこで必要となるのは、こだわりに対する施主のご理解です。\n素材のバランスを吟味し、細部にこだわり、工夫を施すことで\n高価な建材では得られない均衡の取れた落ち着きのある空間を設計します。`}
-            en={`We continue to demonstrate that the quality of space is not always proportional to the budget. \nGood architecture and good space are determined by how uncompromising the design process is and how much time you spend scrutinizing. \n Therefore, what is needed is the owner's understanding of the commitment. \nBy carefully examining the balance of materials, paying attention to details, and devising \n, we design a balanced and calm space that cannot be obtained with expensive building materials.`}
+            ja={`空間の質は、必ずしも予算に比例しないことを実証し続けています。\nよい建築、よい空間は、設計のプロセスでどれほど妥協せず\n時間をかけて吟味し続けるかで決まります。\n素材のバランスを吟味し、細部にこだわり、工夫を施すことで\n高価な建材では得られない均衡の取れた落ち着きのある空間を設計します。`}
+            en={`We continue to demonstrate that the quality of space is not always proportional to the budget.\nGood architecture, good space, It depends on whether you continue to scrutinize over time，never compromise in the design process.\nBy carefully examining the balance of the materials, paying attention to details, \nwe design a balanced and calm space that cannot be obtained only with expensive building materials.`}
+            zh={`我们一贯证明，空间质量并不总是与预算成正比。\n好建筑，好空间，重要的是在设计过程中绝不妥协，\n花费功夫不断的进行思考。\n\n通过认真筛选选材料，仔细考虑细节，\n我们设计出协调，舒适的空间。\n而这种空间用昂贵的建筑材料是无法获得的。`}
           />
         </ConceptText>
-        {
-          isAlt ? <ConceptLayoutImageOuter>
-            <SvgAboutImage0001/>
-          </ConceptLayoutImageOuter> :
-          <ConceptImageOuter>
-            <SvgAboutImage0001/>
-          </ConceptImageOuter>
-        }
       </ConceptItemInner>
     </ConceptItem>
     <ConceptItem>
       <ConceptItemInner>
         <ConceptTitle>
-          <Text ja="人々が繋がりたくなる空間" en="A space where people want to connect" />
+          <Text
+            ja="人々が繋がりたくなる空間"
+            en="A space where people want to connect"
+            zh="衔接人与人的空间"
+          />
         </ConceptTitle>
         <ConceptText language={language}>
           <Text
             ja={`よい空間は、人々の隔たりをなくす力があると感じています。\n豊かな空間では、ひとの心も豊かになり、\nそこにはもてなしと、感謝の気持ちが生まれます。\nオフィスビルのような無味乾燥な空間ではギスギスしがちな関係も、\n 心地よい空間に身を置くことで、互いに許しあいます。\nひとの原点に戻れる場所、そんな空間を設計します。`}
-            en={`I feel that a good space has the power to close people's gaps. \n In a rich space, people's hearts will be enriched, and \n there will be hospitality and gratitude. \n Even in a dry space like an office building, relationships that tend to be squeaky can be forgiven by putting yourself in a comfortable space. \n We will design a space where you can return to the origin of people.`}
+            en={`I feel that a good space has the power to close people's gaps.\nIn a rich space, people's hearts will be enriched,\nHospitality and gratitude are born there.\nRelationships that tend to be squeaky in a dry space like an office building\n By putting yourself in a comfortable space, we forgive each other.\nWe will design a space where people can return to the origin.`}
+            zh={`我觉得一个好的空间可以弥合人们之间的鸿沟。\n在舒适的空间里，人们的心灵将会变得丰富，\n热情好客和感激之情在此诞生。\n在干燥的空间（如办公楼）中往往吱吱作响的人际关系，\n通过将自己置于舒适的空间中，我们可以彼此原谅。\n我们将设计一个空间，让人们可以回到原点。`}
           />
         </ConceptText>
-        {
-          isAlt ? <ConceptLayoutImageOuterRight>
-            <SvgAboutImage0002/>
-          </ConceptLayoutImageOuterRight> :
-          <ConceptImageOuter2>
-            <SvgAboutImage0002/>
-          </ConceptImageOuter2>
-        }
       </ConceptItemInner>
     </ConceptItem>
     <ConceptItem>
       <ConceptItemInner>
         <ConceptTitle>
-          <Text ja="“奥行き”のある空間" en={`Space with "depth"`} />
+          <Text
+            ja="“奥行き”のある空間"
+            en={`Space with "depth"`}
+            zh="具有“深度”的空间" 
+          />
         </ConceptTitle>
         <ConceptText language={language}>
           <Text
-            ja={`素材は、基本的に自然素材を使用します。\nいくら本物の木に似せて作られたプリントのシートでも、\n拡大するとただの凹凸のないまっ平な面です。 \nしかし自然素材は、細胞を内包しており、細胞はさらに構成要素を持っていて\nいくらでも、拡大、縮小可能です。\nその奥行きが、ひとに落ち着きを与え、さらに、\nひとのまわりに必要不可欠な様々な微生物にも\n自然に近い居場所を与えているといえます。\n見た目のクリーンさよりも、調和の取れた空間が、心身の健康につながります。\nそのために、自然素材を多用した奥行のある設計を行います。\nよい建築、よい空間は、設計のプロセスでどれほど妥協せず\n時間をかけて吟味し続けるかで決まります。\nそこで必要となるのは、こだわりに対する施主のご理解です。\n素材のバランスを吟味し、細部にこだわり、工夫を施すことで\n高価な建材では得られない均衡の取れた落ち着きのある空間を設計します。`}
-            en={`The material is basically a natural material. \nNo matter how much a printed sheet is made to resemble a real tree, \nit is just a flat surface with no irregularities when enlarged. \nBut natural materials contain cells, which have additional components that can be expanded and contracted as much as you like. \nIt can be said that its depth gives people calmness, and \n gives a place close to nature to various microorganisms that are indispensable around people. \n A harmonious space, rather than a clean appearance, leads to physical and mental health. \n For that purpose, we will design with depth using a lot of natural materials. \nGood architecture and good space are determined by how uncompromising the design process is and how much time you spend scrutinizing. \n Therefore, what is needed is the owner's understanding of the commitment. \nBy carefully examining the balance of materials, paying attention to details, and devising, \n we design a well-balanced and calm space that cannot be obtained with expensive building materials.`}
+            ja={`素材は、基本的に自然素材を使用します。\nいくら本物の木に似せて作られたプリントのシートでも、\n拡大するとただの凹凸のないまっ平な面です。 \nしかし自然素材は、細胞を内包しており、細胞はさらに構成要素を持っていて\nいくらでも、拡大、縮小可能です。\nその奥行きが、ひとに落ち着きを与え、さらに、\nひとのまわりに必要不可欠な様々な微生物にも\n自然に近い居場所を与えているといえます。\n見た目のクリーンさよりも、調和の取れた空間が、心身の健康につながります。`}
+            en={`The material is basically a natural material.\nNo matter how much the printed sheet is made to resemble a real tree,\nwhen enlarged, it is just a flat surface with no irregularities.\nBut natural materials contain cells, which have more components.\nIt keep the same density as you zoom in and out.\nThe depth gives people a sense of calm, and also for formicroorganisms that are indispensable around people.\nIt can be said that it gives a place close to nature.\nA harmonious space, rather than a clean appearance, leads to physical and mental health.`}
+            zh={`素材基本上是天然材料。\n不管印刷的塑料仿木板多么像真正的木材，\n放大时，它只是一个没有凹凸的平坦表面。\n但是天然材料包含细胞，而细胞具有更小的内部构造。\n随意放大和缩小，它都是有深度的。\n可以说，这种深度给人一种安心舒适的感觉，也给人们提供了一个接近自然的地方，对人有益无害的各种微生物也有了藏身之处。\n真正的身心健康，不是整洁的外面，而是和谐的内面带来的。`}
           />
         </ConceptText>
-        {
-          isAlt ? <ConceptLayoutImageOuter>
-            <SvgAboutImage0003/>
-          </ConceptLayoutImageOuter> :
-          <ConceptImageOuter>
-            <SvgAboutImage0003/>
-          </ConceptImageOuter>
-        }
       </ConceptItemInner>
     </ConceptItem>
   </Outer>
--- src/js/components/flow.tsx
+++ src/js/components/flow.tsx
@@ -24,78 +24,113 @@ export const Flow: FunctionComponent = () => {
   return <Outer>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="01. 無料相談" en="01. Free consultation" />
+        <Text
+          ja="01. 無料相談"
+          en="01. Free consultation"
+          zh="01.免费咨询"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`まずは電話やメールで気軽にご相談ください。\n用途、規模、予算をもとに、\nどのような建物ができるかをご説明いたします。\n敷地が決定している場合は現地調査に伺います。`}
-          en={`Please feel free to contact us by phone or email. \ nWe will explain what kind of building you can build based on the purpose, scale, and budget. \ nIf the site has been decided, we will visit the field survey.`}
+          ja={`まずは電話やメールで気軽にご相談ください。\n用途、規模、予算をもとに、どのような建物ができるかをご説明いたします。\n敷地が決定している場合は現地調査に伺います。`}
+          en={`Please feel free to contact us by phone or email.\nWe will explain what kind of building can be built based on the purpose, scale, and budget.\nIf the site has been decided, we will visit the the site to have a field survey.`}
+          zh={`请由电话或电子邮件与我们联系。\n我们将根据用途，规模和预算来说明可以建造什么样的建筑物。\n如果决定了地点，我们可以去实地调查。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="02. 初回提案" en="02. First proposal" />
+        <Text
+          ja="02. 初回提案"
+          en="02. First proposal"
+          zh="02.概念方案"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`ヒアリングした内容を基に、\nスケッチにて完成像をご提示します。\n用途、規模からおよその予算も算出でき、\n大まかな方向性を掴んでいただけます。`}
-          en={`Based on the contents of the hearing, we will present the completed image with a \ n sketch. \ nYou can calculate the approximate budget from the usage and scale, \ nyou can get a rough idea of ​​the direction.`}
+          ja={`ヒアリングした内容を基に、スケッチにて完成像をご提示します。\n用途、規模からおよその予算も算出でき、大まかな方向性を掴んでいただけます。`}
+          en={`Based on the contents of the hearing, we will present the completed image with a sketch.\nWe can calculate the approximate budget from the usage and scale, and you can get a rough idea of ​​the direction.`}
+          zh={`我们将以草图描绘出大致的想法。\n我们可以根据使用情况和规模来计算大概的预算，您可以大致了解项目的可行性。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="03. 契約" en="03. Agreement" />
+        <Text
+          ja="03. 契約"
+          en="03. Contract"
+          zh="03.签订合同"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`設計契約を行い、いよいよ設計が本格的にスタートします。\n設計費は、床面積規模をもとに算出されますが、\n住宅であれば総工費のおよそ10％程度、\n保育園、クリニックであればおよそ7％程度となります。\n設計費のお支払いは、契約時20％、基本設計完了時30％、\n実施設計完了時30％、竣工時20％をベースとしています。`}
-          en={`After making a design contract, the design will finally start in earnest. \ nThe design cost is calculated based on the floor area scale, but \ nfor a house, it is about 10% of the total construction cost, and \ n for a nursery school or clinic, it is about 7%. \ nPayment of design costs is based on 20% at the time of contract, 30% at the completion of basic design, \ n30% at the completion of implementation design, and 20% at the time of completion.`}
+          ja={`設計契約を行い、いよいよ設計が本格的にスタートします。\n設計費は、床面積規模をもとに算出されますが、住宅であれば総工費のおよそ10％程度、\n保育園、クリニックであればおよそ7％程度となります。\n設計費のお支払いは、契約時20％、基本設計完了時30％、実施設計完了時30％、竣工時20％をベースとしています。`}
+          en={`After making a design contract, the design will be start.\nThe design cost is calculated based on the floor area scale and usage.\nIn the case of a house, it is about 10% of the total construction cost. And in case of a nursery school or clinic, it will be about 7%.\nPayment of design costs is based on 20% at the time of contract, 30% at the completion of schematic design, 30% at the completion of detailed design, and 20% at the time of completion.`}
+          zh={`签订设计合同后，将开始设计。\n设计成本是根据建筑面积比例计算的，如住宅，大约占总建筑成本的10％。如是托儿所或诊所，则约为7％。\n设计费用的支付，一般情况是在合同时20％，基本设计完成时30％，详细设计完成时30％，和完成时20％。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="04. 設計" en="04. design" />
+        <Text
+          ja="04. 設計"
+          en="04. Design"
+          zh="04.设计"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`基本設計段階では、施主のご要望と敷地の周辺環境を考慮し、\n建物の形状や平面計画から進めていきます。\n次に、サンプル等を施主と確認し合いながら、窓などの建材、\nフローリングなどの素材、風呂、キッチンなどの\n設備の仕様を決定していきます。\n実施設計の段階では、決定された建築形状、\n仕上げ、設備機器をもとに、\n工事見積もりや申請用の図面を仕上げていきます。\nまた、詳細の仕様を細かく決めていき、\n空間をより精緻化していきます。`}
-          en={`At the basic design stage, we will proceed from the shape and plan of the building, taking into consideration the request of the owner and the surrounding environment of the site. \ nNext, while checking samples with the owner, we will determine the specifications of building materials such as windows, \ n materials such as flooring, and \ n equipment such as baths and kitchens. \ nIn the implementation design stage, \ nWe will finish the construction estimate and drawings for application based on the determined building shape, \ nfinishing, and equipment. \ nIn addition, we will decide the detailed specifications in detail and make the space more elaborate.`}
+          ja={`基本設計段階では、施主のご要望と敷地の周辺環境を考慮し、建物の形状や平面計画から進めていきます。\n次に、サンプル等を施主と確認し合いながら、窓などの建材、フローリングなどの素材、風呂、キッチンなどの設備の仕様を決定していきます。\n実施設計の段階では、決定された建築形状、仕上げ、設備機器をもとに、工事見積もりや申請用の図面を仕上げていきます。\nまた、詳細の仕様を細かく決めていき、空間をより精緻化していきます。`}
+          en={`At the schematic design stage, we will proceed from the shape and plan of the building in consideration of the owner's request and the surrounding environment of the site.\nNext, while confirming samples with the owner, we will determine the specifications of building parts such as windows, materials such as flooring, and equipment such as baths and kitchens.\nAt the detailed design stage, we will finish the drawings for construction estimate and application based on the determined building spec.\nIn addition, we will decide the detailed specifications and further refine the space.`}
+          zh={`在基本设计阶段，我们将考虑业主的要求和场地周围的环境，从建筑物的形状和平面入手。\n接下来，在与业主确认性能，外观，色彩后，我们将确定各个如窗户之类的建筑部件，如地板之类的材料，以及如浴室和厨房之类的设备的规格。\n在实施设计阶段，我们将根据确定的建筑形状，材料和设备完成施工概算图和申报图。\n同时，我们将详细决定细节的的规格并进一步完善空间质量。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="05. 工事費の見積" en="05. Estimate of construction cost" />
+        <Text
+          ja="05. 工事費の見積"
+          en="05. Estimate of construction cost"
+          zh="05.工程造价估算"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`施工会社に見積り依頼をします。\n建築規模によって何社かに相見積もりを依頼します。\n見積もりを受け取った後、工事金額の妥当性を吟味し比較した後、\n必要に応じて仕様変更を行い、\n最終的に工事金額と施工会社を決定します。\n見積り調整と同時に役所または確認審査機関に申請業務を行います。\n確認申請が発行され次第、着工できます。`}
-          en={`Request an estimate from the construction company. \ n We will ask some companies for a quotation depending on the scale of the building. \ nAfter receiving the quotation, after examining and comparing the validity of the construction amount, \ nchange the specifications as necessary, \ nfinally determine the construction amount and construction company. \ n At the same time as adjusting the estimate, the application will be submitted to the government office or confirmation inspection body. \ n Construction can be started as soon as the confirmation application is issued.`}
+          ja={`施工会社に見積り依頼をします。\n建築規模によって何社かに相見積もりを依頼します。\n見積もりを受け取った後、工事金額の妥当性を吟味し比較した後、必要に応じて仕様変更を行い、最終的に工事金額と施工会社を決定します。\n見積り調整と同時に役所または確認審査機関に申請業務を行います。\n確認申請が発行され次第、着工できます。`}
+          en={`Request an estimate from the construction company.\nWe will ask several companies for a quotation depending on the scale of the building.\nAfter receiving the quotation, we will examine and compare the validity of the construction cost, change the specifications as necessary, and finally decide the construction cost and construction company.\nAt the same time as adjusting the estimate, the application will be submitted to the government office or confirmation inspection body.\nConstruction can begin as soon as the confirmation application is issued.`}
+          zh={`向建筑公司索要估算。\n我们将根据建筑物的规模要求几家公司报价。\n收到报价后，我们将检查并比较建筑成本的有效性，必要时更改规格，最后决定建筑成本和建筑公司。\n在调整工程造价的同时，申报将提交给行政机关或确认检查机构。\n申报许可一经发布，便可以开始施工。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="06. 工事" en="06. Construction" />
+        <Text
+          ja="06. 工事"
+          en="06. Construction"
+          zh="06.施工"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`週に１日程度工事現場にて定例会議を行います。\n工事が図面通りに行われているかを確認し、\n必要に応じて是正指示を行います。\nまた、変更等が必要な場合は図面やスケッチにて補足を行います。\n中間・完了検査の際は必要書類を作成し、検査に立会います。`}
-          en={`Regular meetings are held at the construction site about one day a week. \ nCheck that the construction is done according to the drawing, \ n and give corrective instructions if necessary. \ n Also, if changes are necessary, we will supplement with drawings and sketches. \ n At the time of intermediate / completion inspection, we will prepare the necessary documents and witness the inspection.`}
+          ja={`週に１日程度工事現場にて定例会議を行います。\n工事が図面通りに行われているかを確認し、必要に応じて是正指示を行います。\nまた、変更等が必要な場合は図面やスケッチにて補足を行います。\n中間・完了検査の際は必要書類を作成し、検査に立会います。`}
+          en={`Regular meetings are held at the construction site about one day a week.\nWe will heck that the construction is done according to the drawing, and give corrective instructions if necessary.\nIn addition, if changes are required, we will add drawings and sketches.\nAt the time of intermediate / completion inspection, we will prepare the necessary documents and witness the inspection.`}
+          zh={`每周大约一天在建筑工地开例会。\n检查施工是否按照图纸进行，并在必要时提供修改指示。\n此外，如果需要更改，我们将补充图纸和草图。\n在进行中间/完成检查时，我们将准备必要的文件并见证检查。`}
         />
       </ConceptText>
     </FlowItem>
     <FlowItem>
       <ConceptTitle>
-        <Text ja="07. 検査、引渡し" en="07. Inspection, delivery" />
+        <Text
+          ja="07. 検査、引渡し"
+          en="07. Inspection and handover"
+          zh="07.检验和交付"
+          />
       </ConceptTitle>
       <ConceptText language={language}>
         <Text
-          ja={`設計者、施工者、お客様の三者立ち会いのもと、\n施工不良や汚れなどの検査を行い、\n修正を行った後にお引渡しとなります。\nまた、設備等の使い方のご説明も行われます。\n自然素材を使用する場合は特に、建材は生きていて、\n時間経過とともに状態は変化します。\nお住まいになってから通常一年経過した時点で、\n一年検査を施工者が行い、不具合を是正します。`}
-          en={`In the presence of the designer, the builder, and the customer, \ ninspect the construction defects and dirt, and \ ncorrect it before handing it over. \ n Also, explanations on how to use the equipment will be given. \ nThe building materials are alive, especially when using natural materials, \ n and their condition changes over time. \ n Normally, one year after you live, \ n the contractor will perform a one-year inspection to correct any problems.`}
+          ja={`設計者、施工者、お客様の三者立ち会いのもと、施工不良や汚れなどの検査を行い、修正を行った後にお引渡しとなります。\nまた、設備等の使い方のご説明も行われます。\n自然素材を使用する場合は特に、建材は生きていて、時間経過とともに状態は変化します。\nお住まいになってから通常一年経過した時点で、一年検査を施工者が行い、不具合を是正します。`}
+          en={`In the presence of the designer, the builder, and the customer, we will inspect for construction defects and dirt, make corrections, and then hand over the building.\nIn addition, explanations on how to use the equipment will be given.\nBuilding materials are alive, especially when using natural materials, and their condition changes over time.\nUsually, one year after you live, the contractor will perform a one-year inspection to correct any problems.`}
+          zh={`在设计师，建筑商和客户在场的情况下，我们将检查施工缺陷和污垢，进行更正，然后移交建筑物。\n此外，还将对设备等做使用说明。\n建筑材料是活的，尤其是在使用天然材料时，它们的状况会随着时间而变化。\n通常，在您居住一年后，承包商将进行竣工一年后检查以纠正问题。`}
         />
       </ConceptText>
     </FlowItem>
