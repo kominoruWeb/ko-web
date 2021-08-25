5a92d0f678c693e151dbf2d5f0bc0ffc6541d305
piou <piou@hotmail.co.jp>
2021-08-25 10:16:08 +0900

    Update

--- src/static/load.js
+++ src/static/load.js
@@ -4,7 +4,7 @@ if(!!window.MSInputMethodContext && !!document.documentMode){
     <script src="https://unpkg.com/whatwg-fetch@3.6.2/dist/fetch.umd.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js"></script>
     <script src="https://unpkg.com/@babel/standalone@7.15.3/babel.js"></script>
-    <script type="text/babel" src="index.js"></script>
+    <script type="text/babel" src="/index.js"></script>
     <script>alert('このブラウザはサポートされていません')</script>
   `)
 } else {
